460f7007557063e4b9a988023aef198649cdb927
Wooseong <57824259+yoonwooseong@users.noreply.github.com>
2021-07-09 16:25:23 +0900

    Deposit 테스트코드 작성

--- src/main/java/bis/stock/back/domain/asset/DepositRepository.java
+++ src/main/java/bis/stock/back/domain/asset/DepositRepository.java
@@ -1,9 +1,12 @@
 package bis.stock.back.domain.asset;
 
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import bis.stock.back.domain.asset.dto.Deposit;
+import bis.stock.back.domain.user.User;
 
 public interface DepositRepository extends JpaRepository<Deposit, Long>{
-
+	Optional<User> findById(String user_id);
 }
