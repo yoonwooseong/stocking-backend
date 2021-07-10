65bd00c2d3caa54e2a25868a23778c913b2d7673
s4ng <zalcls951201@gmail.com>
2021-07-10 16:33:37 +0900

    add user controller test

--- src/main/java/bis/stock/back/domain/user/dto/UserUpdateDto.java
+++ src/main/java/bis/stock/back/domain/user/dto/UserUpdateDto.java
@@ -1,8 +1,14 @@
 package bis.stock.back.domain.user.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Getter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserUpdateDto {
 
     private String email;
