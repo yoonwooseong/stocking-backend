2ef82783e3d0d855fab19dbfc1b742d0ef6472cb
yoonwooseong <57824259+yoonwooseong@users.noreply.github.com>
2021-12-07 19:29:05 +0900

    Update WithdrawRepository.java

--- src/main/java/bis/stock/back/domain/asset/WithdrawRepository.java
+++ src/main/java/bis/stock/back/domain/asset/WithdrawRepository.java
@@ -3,7 +3,11 @@ package bis.stock.back.domain.asset;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import bis.stock.back.domain.asset.dto.Withdraw;
+import org.springframework.data.jpa.repository.Query;
 
 public interface WithdrawRepository extends JpaRepository<Withdraw, Long>{
 	List<Withdraw> findAllById(Long user_id);
+
+//	@Query("select w.username from Withdraw w")
+//	List<String> findUsernameList();
 }
